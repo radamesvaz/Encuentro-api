06d2862db8cc9f71dde0ff921d6751c131b846b6
Radames Vaz <rgvtovar@hotmail.com>
2020-07-21 17:40:41 -0400

    comenzando proyecto

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql');
+const fs = require('fs');
+require('dotenv').config();
+const knex = require('knex');
+
+
+// LLamando a los controladores
+const home = require('./controllers/Home');
+
+const db = knex({
+    client: 'mysql',
+    connection: {
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      port: 3306,
+      database: process.env.DATABASE
+    }
+  });
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+  
+//Inicio de endpoints
+
+app.get('/', (req, res) => {res.json('estoy vivo!')});
+
+//Obtener todos los productos
+app.get('/home', (req, res) => { home.handleHome(req, res, db) });
+
+
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`I'm alive here ${port}`))
