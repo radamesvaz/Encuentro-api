9b3a0a503eff3f18f65185322082e7b804ff32f7
Radames Vaz <rgvtovar@hotmail.com>
2020-08-07 22:30:59 -0400

    cambiando home posts

--- controllers/HomePosts.js
+++ controllers/HomePosts.js
@@ -1,7 +1,16 @@
 const handleHomePosts = (req, res, db) => {
     db.select().table('posts')
     .then(response => {
-        res.json(response);
+        return [].slice.call(arguments).sort(function(a,b){ 
+            return b - a; 
+          }); 
+
+        //const ordenadoFecha = response.fecha.sort()
+        /*
+        for(let i = 0; i < response.length; i++){
+            console.log(response[i].fecha.sort())
+        }*/
+        res.json(response)
     })
 .catch(err => res.status(500).json('problema con la base de datos + ' + err))
 }
